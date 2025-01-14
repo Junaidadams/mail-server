fa25d7b6fd08535f0354d7672e295a3aa50b3a10
Junaid Adams <junaidadams117@gmail.com>
2025-01-14 15:43:29 +0200

    Fixing cors policy error

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: ["http://localhost:5173"],
-    origin: ["http://localhost:5175"],
+    origin: ["http://localhost:5175/"],
     credentials: true,
   })
 );
