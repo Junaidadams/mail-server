4fab88f3e171d4edffae125cb55a6f49eb288bd0
Junaid Adams <junaidadams117@gmail.com>
2026-08-30 19:45:39 +0200

    added junaidadams.com

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ app.use(
       "http://localhost:5173",
       "http://localhost:5174",
       "http://localhost:5175",
+      "https://junaidadams.com",
+      "www.junaidadams.com",
       "https://roob.online",
       "www.roob.online",
     ],
